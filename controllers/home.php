0b4ec4ed7697566b0fb02002b00c0532a41ecf2e
obvioulescence <clementcolle@free.fr>
2013-12-19 20:16:56 +0100

    Ajout du generateur d'article dans le la vue home

--- controllers/home.php
+++ controllers/home.php
@@ -1,10 +1,6 @@
 <?php if(!defined('APPPATH')) exit('You shouldn\'t have seen this, htaccess removed OR APPATH removed in /index.php');
 
  // Libs ans Models Automaticly loaded at APPPATH/libs and APPPATH/models if names are the identicals 
- 
- $unAutreTableau = array(
-	'helloMotherFuckingWorld' => getHelloWorld() // function du modele chargé automatiquement
- );
 
  
  // je n'ai pas mis d'exemple de lib pour pas foutre toute la comprhéension en l'air :) 
@@ -13,8 +9,52 @@
  // addInformation('Les truc de notif sont good', $infosHeader);
  // addError('on peut même dire qu\'il est pas content', $infosHeader);
  
+ $dataHome = array(
+	'recent_articles' => 'à faire',
+	'articles' => array(
+		0 => array(
+			'image'    => 'http://lorempixel.com/650/300/',
+			'author'   => 'loremPixel',
+			'date'     => 'il y a 10 heures',
+			'title'    => 'Un générateur d\'image :)',
+			'content'  => 'Nous voyons à ce jour apparaitre un MotherFucking générateur super extra cool : j\'ai nommé LoremPixel. Il génère Automatiquement et aléatoirement des images de la taille de votre choix. Très pratique pour par exemple illustrer un blog en construction comme l\'incroyable BlackWave qui [...]',
+			'comments' => array(
+				0 => array(
+					'author'  => 'Amazing',
+					'date'    => 'il y a 2 secondes',
+					'content' => 'That fucking awsome Man, i want this so hard !'
+				),
+				1 => array(
+					'author'  => 'notbadguy',
+					'date'    => 'il y a 12 minutes',
+					'content' => 'ça c\'est plutot sympa ;)'
+				)
+			)
+		),
+		1 => array(
+			'image'    => 'http://placekitten.com/650/200',
+			'author'   => 'KittyFanBoy',
+			'date'     => 'il y a 3 heures',
+			'title'    => 'Un chaton très con',
+			'content'  => 'Le chat domestique (Felis silvestris catus) est un mammifère carnivore de la famille des félidés. Il est l’un des principaux animaux de compagnie et compte aujourd’hui une cinquantaine de races différentes reconnues par les instances de certification. Dans de nombreux pays, le chat entre dans le cadre de la législation sur les carnivores domestiques à l’instar du chien et [...]',
+			'comments' => array(
+				0 => array(
+					'author'  => 'An@rchiste',
+					'date'    => 'il y a 2 secondes',
+					'content' => 'brûlons ces maudites créatures !'
+				),
+				1 => array(
+					'author'  => 'Egyptocrate',
+					'date'    => 'il y a 1 minute',
+					'content' => 'Que dieu protège ce malheureux'
+				)
+			)
+		)
+	)
+ );
+ 
  // okay appelons la vue
  callView('header', $infosHeader);
- callView('home'  , $unAutreTableau); // sera sous la forme $non ou $helloMotherFuckingWorld, elle sont reinterpretées
+ callView('home'  , $dataHome); // sera sous la forme $non ou $helloMotherFuckingWorld, elle sont reinterpretées
 
 ?>
--- views/home.php
+++ views/home.php
@@ -29,29 +29,37 @@
 		</section>
 
 		<section id="content_articles" class="right">
+			
+		<?php
+			foreach($articles as $article) // for every articles
+			{
+		?>
 			<article class="article">
 				<!-- image article -->
 				<div class="bloc_image_article">
-					<img src="http://placekitten.com/650/200" alt="">
+					<img src="<?php echo $article['image']; ?>" alt="">
 				</div>
 				<!-- contenu article -->
 				<div class="apercus_infos_article">
 					<p class="bloc_infos_article right">
-						<a href="#"><span class="icon-users"></span> Auteur</a> - 
-						<span class="icon-calendar"></span> Date
+						<a href="#"><span class="icon-users"></span> <?php echo $article['author']; ?></a> - 
+						<span class="icon-calendar"></span> <?php echo $article['date']; ?>
 					</p>
-					<h1><a href="#">Titre Article</a></h1>
-					<p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte [...]</p>
+					<h1><a href="#"><?php echo $article['title']; ?></a></h1>
+					<p><?php echo $article['content']; ?></p>
 					<hr>
 					<!-- commentaires -->
+			<?php 
+				foreach($article['comments']as $comment)
+				{
+			?>
 					<div class="commentaire">
-						<p class="infos_commentaire">Auteur <span class="right"><span class="icon-calendar"></span> Date</span></p>
-						<p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500</p>
+						<p class="infos_commentaire"><?php echo $comment['author']; ?> <span class="right"><span class="icon-calendar"></span> <?php echo $comment['date']; ?></span></p>
+						<p><?php echo $comment['content']; ?></p>
 					</div>
-					<div class="commentaire">
-						<p class="infos_commentaire">Auteur <span class="right"><span class="icon-calendar"></span> Date</span></p>
-						<p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500</p>
-					</div>
+			<?php
+				}
+			?>
 					<hr>
 					<!-- boutons commentaire -->
 					<p>
@@ -63,8 +71,10 @@
 						<a class="right" href="#"><span class="icon-comments"></span> 20</a>
 					</p>
 				</div>
-
 			</article>
+		<?php
+			} // end foreach
+		?>
 		</section>
 <?php
 
