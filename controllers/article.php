70c0dcdb13611e61500858b7a394349c23e30ebc
obvioulescence <clementcolle@free.fr>
2014-01-09 16:56:55 +0100

    commit commentaire et legere retouche

--- controllers/article.php
+++ controllers/article.php
@@ -1,14 +1,29 @@
 <?php if(!defined('APPPATH')) exit('You shouldn\'t have seen this, htaccess removed OR APPATH removed in /index.php');
 
-$article = getArticle(intval($_GET['id']));
+ /*************************************
+ *               INIT			      *     
+ *************************************/
 
+if(!is_numeric($_GET['id'])) // if the id is not numeric, well it's not an id
+	show404();
+
+$article = getArticle(intval($_GET['id'])); //we load the article
+
+if(empty($article)) // if the article does not match : id does not exist
+	show404();
+
+	// array given to the view
 $dataHome = array(
 	'article' => $article,
-	'similar_articles' => getSimilar($article['id_cat'] ,0 , 5),
-	'comments' => getComments(intval($_GET['id']))
+	'similar_articles' => getSimilar($article['id_cat'] ,0 , 5), // get "tu va kiffer" selection
+	'comments' => getComments(intval($_GET['id'])) // fetch all comment usable for this article
  );
 
-if(isset($_GET['notif']))
+ /*************************************
+ *           NOTIFICATIONS		      *     
+ *************************************/
+ 
+if(isset($_GET['notif'])) // if we have to display some notifications
 {
  	if($_GET['notif'] == 'editArticle')
  		addValidation('Votre article a bien été modifié !', $infosHeader);
@@ -20,14 +35,17 @@ if(isset($_GET['notif']))
  		addValidation('Votre commentaire a bien été ajouté !', $infosHeader);
 }
 
+ /*************************************
+ *               Actions		      *     
+ *************************************/
 
-
-
-
+ //if the guy want to do something with comment and is connected
 if(isset($_GET['comment']) && !empty($_SESSION['id_user']))
 {
+	// if he want to add one and $_POST something out
 	if($_GET['comment'] == 'add' && !empty($_POST['content']))
 	{
+		//well we can add his comment and tell him thats done or not
 		if(addComment($article['id_article'], $_POST['content']))
 		{
 			header("Location: index.php?id=".$article['id_article']."&action=article&notif=addComment");
@@ -36,11 +54,13 @@ if(isset($_GET['comment']) && !empty($_SESSION['id_user']))
 		else
 			addError("OOppps, cette erreur est normalement impossible", $infosHeader);
 	}
-	else if(!empty($article['isAuthor']) || $_SESSION['rank'] == 1)
+	// if this guy want dit or delete, he had to be the author or admin
+	else if(!empty($article['isAuthor']) || $_SESSION['rank'] == 1) 
 	{
+		//well that's kind of simple, we delete the '$_GET['commentId']' comment
 		if($_GET['comment'] == 'delete' && !empty($_GET['commentId']))
 		{
-			//s'il est valide et a lui
+			//lets delete
 			if(deleteComment($_GET['commentId']))
 			{
 				header("Location: index.php?id=".$article['id_article']."&action=article&notif=deleteComment");
@@ -49,6 +69,7 @@ if(isset($_GET['comment']) && !empty($_SESSION['id_user']))
 			else
 				addError("OOppps, cette erreur est normalement impossible", $infosHeader);
 		}
+		//he had to be the author or admin too
 		else if($_GET['comment'] == 'edit' && !empty($_GET['commentId']) && !empty($_POST['content']))
 		{
 			//s'il est valide et a lui
@@ -62,6 +83,10 @@ if(isset($_GET['comment']) && !empty($_SESSION['id_user']))
 		}
 	}
 }
+
+ /*************************************
+ *             LETS BOOT		      *     
+ *************************************/
 
 callView('header', $infosHeader);
 callView('article', $dataHome);
--- controllers/editArticle.php
+++ controllers/editArticle.php
@@ -1,5 +1,9 @@
 <?php if(!defined('APPPATH')) exit('You shouldn\'t have seen this, htaccess removed OR APPATH removed in /index.php');
 
+ /*************************************
+ *               INIT			      *     
+ *************************************/
+
 $dataHome = array(
 	'categories' => getCategories(),
 	'article' => array(
@@ -10,137 +14,140 @@ $dataHome = array(
 	)
 );
 
-if((!empty($_SESSION['id_user']) && $_SESSION['rank'] == 2) || (!empty($_SESSION['id_user']) && $_SESSION['rank'] == 1))
+ /*************************************
+ *               Actions		      *     
+ *************************************/
+//if he (is connected and he is a blogger) or (is connected and admin)
+if(!((!empty($_SESSION['id_user']) && $_SESSION['rank'] == 2) || (!empty($_SESSION['id_user']) && $_SESSION['rank'] == 1)))
 {
-	// if add an article
-	if(isset($_GET['statment']) && ($_GET['statment'] == 'new'))
+	header("Location: index.php");
+	exit;
+}
+
+// if add an article
+if(isset($_GET['statment']) && ($_GET['statment'] == 'new'))
+{
+	if(!empty($_POST))
 	{
+		$dataHome = array(
+			'categories' => getCategories(),
+			'article' => array(
+				'title' => $_POST['title'],
+				'id_cat' => $_POST['id_cat'],
+				'content' => $_POST['content']
+				)
+		 );
+
+		$errors = isValid($_POST); // check if form is valid -> return $errors array with errors
+		$valid = true;
+
+		foreach ($errors as $value) 
+		{
+				addError($value, $infosHeader); // insert error message
+				$valid = false;
+		}
+
+		if($valid) // if not errors
+		{
+			if(addArticle($_POST))
+			{
+				// add article in bdd
+				header("Location: index.php?notif=newArticle");
+				exit;
+			}
+		}
+	}
+}
+
+// if edit article
+if(isset($_GET['statment']) && ($_GET['statment'] == 'edit'))
+{
+
+	$infosArticle = getArticle($_GET['id_article']);
+	$idUser_article = $infosArticle['id_user'];
+
+	// if is author -> update
+	if($idUser_article == $_SESSION['id_user'] || $_SESSION['rank'] == 1) 
+	{
+
+		$dataHome = array(
+		'categories' => getCategories(),
+		'article' => array(
+			'id_article' => $infosArticle['id_article'],
+			'image' => $infosArticle['image'],
+			'title' => $infosArticle['title'],
+			'id_cat' => $infosArticle['id_cat'],
+			'content' => $infosArticle['content']
+			)
+		);
+
 		if(!empty($_POST))
 		{
-			$dataHome = array(
-				'categories' => getCategories(),
-				'article' => array(
-					'title' => $_POST['title'],
-					'id_cat' => $_POST['id_cat'],
-					'content' => $_POST['content']
-					)
-			 );
-
 			$errors = isValid($_POST); // check if form is valid -> return $errors array with errors
 			$valid = true;
 
 			foreach ($errors as $value) 
 			{
+				if(!empty($value)) // if error exist
+				{
 					addError($value, $infosHeader); // insert error message
 					$valid = false;
+				}
 			}
+
+			// new value 
+			$nouv = array(
+				'id_article' => $infosArticle['id_article'],
+				'title' => htmlspecialchars($_POST['title']),
+				'id_cat' => htmlspecialchars($_POST['id_cat']),
+				'content' => $_POST['content']
+			);
+
+			$dataHome = array(
+				'categories' => getCategories(),
+				'article' => $nouv
+			 );
 
 			if($valid) // if not errors
 			{
-				if(addArticle($_POST))
+				if(editArticle($_POST))
 				{
 					// add article in bdd
-					header("Location: index.php?notif=newArticle");
+					header("Location: index.php?action=article&id=".$_POST['id_article']."&notif=editArticle");
 					exit;
 				}
 			}
 		}
 	}
-
-	// if edit article
-	if(isset($_GET['statment']) && ($_GET['statment'] == 'edit'))
+	// if not author
+	else
 	{
-
-		$infosArticle = getArticle($_GET['id_article']);
-		$idUser_article = $infosArticle['id_user'];
-
-		// if is author -> update
-		if($idUser_article == $_SESSION['id_user'] || $_SESSION['rank'] == 1) 
-		{
-
-			$dataHome = array(
-			'categories' => getCategories(),
-			'article' => array(
-				'id_article' => $infosArticle['id_article'],
-				'image' => $infosArticle['image'],
-				'title' => $infosArticle['title'],
-				'id_cat' => $infosArticle['id_cat'],
-				'content' => $infosArticle['content']
-				)
-		 	);
-
-			if(!empty($_POST))
-			{
-				$errors = isValid($_POST); // check if form is valid -> return $errors array with errors
-				$valid = true;
-
-				foreach ($errors as $value) 
-				{
-					if(!empty($value)) // if error exist
-					{
-						addError($value, $infosHeader); // insert error message
-						$valid = false;
-					}
-				}
-
-				// new value 
-				$nouv = array(
-					'id_article' => $infosArticle['id_article'],
-					'title' => htmlspecialchars($_POST['title']),
-					'id_cat' => htmlspecialchars($_POST['id_cat']),
-					'content' => $_POST['content']
-				);
-
-				$dataHome = array(
-					'categories' => getCategories(),
-					'article' => $nouv
-				 );
-
-				if($valid) // if not errors
-				{
-					if(editArticle($_POST))
-					{
-						// add article in bdd
-						header("Location: index.php?action=article&id=".$_POST['id_article']."&notif=editArticle");
-						exit;
-					}
-				}
-			}
-		}
-		// if not author
-		else
-		{
-			header("Location: index.php?action=home");
-			exit;
-		}
+		header("Location: index.php?action=home");
+		exit;
 	}
-
-	// if delete article
-	if(isset($_GET['statment']) && ($_GET['statment'] == 'delete'))
-	{
-		$infosArticle = getArticle($_GET['id_article']);
-		$idUser_article = $infosArticle['id_user'];
-
-		if($idUser_article == $_SESSION['id_user'] || $_SESSION['rank'] == 1)
-		{
-			deleteArticle(htmlspecialchars($_GET['id_article']));
-			header("Location: index.php?action=home&notif=deleteArticle");
-			exit;
-		}
-		else
-		{
-			header("Location: index.php?action=home");
-			exit;
-		}
-	}
-
-	callView('header', $infosHeader);
-	callView('editArticle', $dataHome);
-	callView('footer');
 }
-else
+
+// if delete article
+if(isset($_GET['statment']) && ($_GET['statment'] == 'delete'))
 {
-	header("Location: index.php");
-	exit;
+	$infosArticle = getArticle($_GET['id_article']);
+	$idUser_article = $infosArticle['id_user'];
+
+	if($idUser_article == $_SESSION['id_user'] || $_SESSION['rank'] == 1)
+	{
+		deleteArticle(htmlspecialchars($_GET['id_article']));
+		header("Location: index.php?action=home&notif=deleteArticle");
+		exit;
+	}
+	else
+	{
+		header("Location: index.php?action=home");
+		exit;
+	}
 }
+
+callView('header', $infosHeader);
+callView('editArticle', $dataHome);
+callView('footer');
+
 ?>
